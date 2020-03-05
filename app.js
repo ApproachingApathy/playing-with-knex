3feb0876d5d88e0ab4b3b89d9b766ed3877eddc7
Ephriam Henderson <hendersonephriamn@gmail.com>
2020-03-05 11:08:07 -0600

    Set up app.js
    - Set up config file
    - Set up logger

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+const winston = require("winston");
+
+global.appConfig = require("./config.json");
+global.logger = winston.createLogger({
+	level: appConfig.LOGGER_LEVEL,
+	transports: [
+		new winston.transports.Console({
+			level: "debug",
+			format: winston.format.cli()
+		}),
+		new winston.transports.File({
+			filename: "info.log",
+			level: "info",
+			format: winston.format.json()
+		}),
+		new winston.transports.File({
+			filename: "error.log",
+			level: "error",
+			format: winston.format.json()
+		})
+	]
+});
+
+const server = app.listen(appConfig.PORT, appConfig.SERVER_ADDRESS, () => {
+	const host = server.address().address;
+	logger.debug(host);
+	const port = server.address().port;
+	logger.info(`Server started at http://${host}:${port}`);
+});
